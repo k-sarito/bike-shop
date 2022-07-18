8c7e23ac7718d0df8c37e4831cafcdd1d6df96c8
k-sarito <coryclark95@gmail.com>
2022-07-18 14:09:06 -0500

    repository method

--- BiancasBikeShop/Repositories/BikeRepository.cs
+++ BiancasBikeShop/Repositories/BikeRepository.cs
@@ -18,7 +18,45 @@ namespace BiancasBikeShop.Repositories
         public List<Bike> GetAllBikes()
         {
             var bikes = new List<Bike>();
-            // implement code here... 
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT b.Id, b.Brand, b.Color, b.OwnerId, b.BikeTypeId, o.Name AS OwnerName, o.Address, o.Email, o.Telephone, bt.Name AS TypeName
+                        FROM Bike as b
+                        LEFT JOIN Owner as o ON o.Id = b.OwnerId
+                        LEFT JOIN BikeType as bt ON bt.Id = b.BikeTypeId";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Bike bike = new Bike
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Brand = reader.GetString(reader.GetOrdinal("Brand")),
+                                Color = reader.GetString(reader.GetOrdinal("Color")),
+                                Owner = new Owner
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                    Name = reader.GetString(reader.GetOrdinal("OwnerName")),
+                                    Address = reader.GetString(reader.GetOrdinal("Address")),
+                                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                                    Telephone = reader.GetString(reader.GetOrdinal("Telephone"))
+                                },
+                                BikeType = new BikeType
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("BikeTypeId")),
+                                    Name = reader.GetString(reader.GetOrdinal("TypeName"))
+                                }
+
+                            };
+                            bikes.Add(bike);
+                        }
+                    }
+                }
+            }
             return bikes;
         }
 
